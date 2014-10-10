c20da0c9d199aba011f0ca0f192f517cda357fcc
mntj <mntj11@gmail.com>
2014-10-10 12:53:28 -0400

    get_yahoo_finance_data test passing

--- lib/stock_reader.rb
+++ lib/stock_reader.rb
@@ -6,4 +6,9 @@ module StockReader
     end
     ticker_array
   end
+
+  def self.get_yahoo_finance_data(ticker_array)
+    params = [:name, :symbol, :market_capitalization, :ebitda, :earnings_per_share, :book_value]
+    data = YahooFinance.quotes(ticker_array, params)
+  end
 end
--- spec/lib/stock_reader_spec.rb
+++ spec/lib/stock_reader_spec.rb
@@ -4,13 +4,22 @@ require 'stock_reader'
 RSpec.describe StockReader do
 
   describe 'extract_tickers' do
-    it 'returns an array of ticker symbols' do
+    it 'returns a correct array of ticker symbols' do
       test_file = File.open("test_file.txt", "w")
       File.readlines("russell3000.txt").take(4).each { |line| test_file.write(line) }
       test_file.rewind
       expected = ['FLWS', 'SRCE', 'FUBC', 'FOXA']
       expect(StockReader.extract_tickers(test_file)).to eq(expected)
       File.delete("test_file.txt")
+    end
+  end
+
+  describe 'get_yahoo_finance_data' do
+    it 'returns YahooFinance data' do
+      test_tickers = ['FLWS', 'SRCE', 'FUBC', 'FOXA']
+      actual = StockReader.get_yahoo_finance_data(test_tickers)[0].class.to_s
+      expected = "OpenStruct"
+      expect(actual).to eq(expected)
     end
   end
 
