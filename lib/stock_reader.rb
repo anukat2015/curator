8ceb802043997c5d5e3bf77d173b6456981ef653
mntj <mntj11@gmail.com>
2014-10-10 01:24:12 -0400

    Failing test for extract_tickers method

--- lib/stock_reader.rb
+++ lib/stock_reader.rb
@@ -2,4 +2,7 @@ module StockReader
   def self.adder(a,b)
     a+b
   end
+
+  def self.extract_tickers(file)
+  end
 end
--- spec/lib/stocker_reader_spec.rb
+++ spec/lib/stocker_reader_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe StockReader do
       expect(StockReader.adder(1,2)).to eq(3)
     end
   end
+
+  describe 'extract_tickers' do
+    it 'returns an array of ticker symbols' do
+      test_file = File.open("test_file.txt", "w")
+      File.readlines("russell3000.txt").take(4).each { |line| test_file.write(line) }
+      test_file.rewind
+      expected = ['FLWS', 'SRCE', 'FUBC', 'FOXA']
+      expect(StockReader.extract_tickers(test_file)).to eq(expected)
+    end
+  end
+
 end
