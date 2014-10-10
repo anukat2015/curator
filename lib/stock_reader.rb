1fe0b1fcf35d110fd96b9dedd17e6741f1abec29
mntj <mntj11@gmail.com>
2014-10-10 11:51:46 -0400

    extract_tickers method test passing

--- lib/stock_reader.rb
+++ lib/stock_reader.rb
@@ -1,4 +1,9 @@
 module StockReader
-  def self.extract_tickers(file)
+  def self.extract_tickers(ticker_file)
+    ticker_array = []
+    File.readlines(ticker_file).each do |line|
+      ticker_array << line.match(/\s(\w+)$/).to_s.strip
+    end
+    ticker_array
   end
 end
--- spec/lib/stock_reader_spec.rb
+++ spec/lib/stock_reader_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 require 'stock_reader'
 
 RSpec.describe StockReader do
-  describe 'adder' do
-    it 'returns the sum of its arguments' do
-      expect(StockReader.adder(1,2)).to eq(3)
-    end
-  end
 
   describe 'extract_tickers' do
     it 'returns an array of ticker symbols' do
