0a55a876f9e2531f82e416094ba8e3e0997d8636
mntj <mntj11@gmail.com>
2014-10-13 17:56:17 -0400

    Corrected format for failing earnings_yield test

--- spec/lib/stock_reader_spec.rb
+++ spec/lib/stock_reader_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe StockReader do
       test_tickers = ['FLWS', 'SRCE', 'FOXA']
       actual = StockReader.get_earnings_yield(test_tickers)
       expected = [{company: "FLWS",
-                   enterprise_value: {"FLWS_EV" => ["2013-06-27", 462.7165021151086]},
-                   ebit: {"FLWS_EBIT_1T" => ["2013-06-27", 21.649862485391672]},
+                   enterprise_value: ["2013-06-27", 462.7165021151086],
+                   ebit: ["2013-06-27", 21.649862485391672],
                    earnings_yield: 0.046788611139712284
                    },
                    {company: "SRCE",
-                    enterprise_value: {"SRCE_EV" => ["2012-12-31", 1051.297512924382]},
-                    ebit: {"SRCE_EBIT_1T" => ["2012-12-31", -0.2380449344876999]},
+                    enterprise_value: ["2012-12-31", 1051.297512924382],
+                    ebit: ["2012-12-31", -0.2380449344876999],
                     earnings_yield: -0.00022642965626878833
                    }]
       expect(actual).to eq(expected)
