97726dc38b7a268d40db04919c910ab7d01b8f5d
mntj <mntj11@gmail.com>
2014-10-14 15:56:58 -0400

    Generate CompanyReportMailer

---
+++ app/mailers/company_report_mailer.rb
@@ -0,0 +1,3 @@
+class CompanyReportMailer < ActionMailer::Base
+  default from: "from@example.com"
+end
---
+++ spec/mailers/company_report_mailer_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe CompanyReportMailer, :type => :mailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
