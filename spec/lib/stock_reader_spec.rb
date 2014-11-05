339d3204c87c6a2450bdfba42976aacf6528ccb8
mntj <mntj11@gmail.com>
2014-11-04 21:56:56 -0500

    Put sample data for specs into its own file

--- spec/lib/stock_reader_spec.rb
+++ spec/lib/stock_reader_spec.rb
@@ -1,65 +1,8 @@
 require 'spec_helper'
 require 'stock_reader'
+require 'sample_data'
 
 RSpec.describe StockReader do
-
-  def earnings_yield_data_1
-    {
-                       :symbol => "AAPL",
-                   :total_debt => 31040000000.0,
-              :total_debt_date => "2014-06-28",
-                   :market_cap => 581960793730.0,
-              :market_cap_date => "2014-07-23",
-         :cash_and_equivalents => 12977000000.0,
-    :cash_and_equivalents_date => "2014-06-28",
-                         :ebit => 10484000000.0,
-                    :ebit_date => "2014-06-28",
-             :enterprise_value => 600023793730.0,
-               :earnings_yield => 0.017472640434518523
-    }
-  end
-
-  def earnings_yield_data_2
-    {
-                       :symbol => "SRCE",
-                   :total_debt => 468492000.0,
-              :total_debt_date => "2014-06-30",
-                   :market_cap => 689213538.87,
-              :market_cap_date => "2014-07-24",
-         :cash_and_equivalents => 117378000.0,
-    :cash_and_equivalents_date => "2014-06-30",
-                         :ebit => 24110000.0,
-                    :ebit_date => "2014-06-30",
-             :enterprise_value => 1040327538.8699999,
-               :earnings_yield => 0.023175393420987584
-    }
-  end
-
-  def return_on_capital_data_1
-    {
-                  :symbol => "AAPL",
-            :total_assets => 222520000000.0,
-       :total_assets_date => "2014-06-28",
-          :current_assets => 67949000000.0,
-     :current_assets_date => "2014-06-28",
-         :working_capital => 21744000000.0,
-    :working_capital_date => "2014-06-28",
-       :return_on_capital => 0.05946175878399455
-    }
-  end
-
-  def return_on_capital_data_2
-    {
-                  :symbol => "AIR",
-            :total_assets => 2199500000.0,
-       :total_assets_date => "2014-05-31",
-          :current_assets => 1116900000.0,
-     :current_assets_date => "2014-05-31",
-         :working_capital => 714800000.0,
-    :working_capital_date => "2014-05-31",
-       :return_on_capital => 0.01874930455101814
-    }
-  end
 
   describe '.extract_tickers' do
     it 'returns a correct array of ticker symbols' do
