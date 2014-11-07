0f71584ba31d59b18d3b56c17ee7074d975a6e40
mntj <mntj11@gmail.com>
2014-11-06 19:19:28 -0500

    Failing test for .calculate_ey_and_roc

--- spec/lib/stock_reader_spec.rb
+++ spec/lib/stock_reader_spec.rb
@@ -53,6 +53,46 @@ RSpec.describe StockReader do
     end
   end
 
+  describe '.calculate_ey_and_roc' do
+    it 'calculates properly' do
+      ey_hash =
+      {
+         :symbol => "AAPL",
+         :ebit => 10484000000.0,
+         :ebit_date => "2014-06-28",
+         :market_cap => 581960793730.0,
+         :market_cap_date => "2014-07-23",
+         :cash_and_equivalents => 12977000000.0,
+         :cash_and_equivalents_date => "2014-06-28",
+         :total_debt => 31040000000.0,
+         :total_debt_date => "2014-06-28"
+      }
+      roc_hash =
+      {
+        :symbol => "AAPL",
+         :ebit => 10484000000.0,
+         :ebit_date => "2014-06-28",
+         :total_assets => 222520000000.0,
+         :total_assets_date => "2014-06-28",
+         :current_assets => 67949000000.0,
+         :current_assets_date => "2014-06-28",
+         :working_capital => 21744000000.0,
+         :working_capital_date => "2014-06-28"
+      }
+
+      completed_ey_hash = StockReader.calculate_ey_and_roc(ey_hash)
+      completed_roc_hash = StockReader.calculate_ey_and_roc(roc_hash)
+      ev = (ey_hash[:market_cap] + ey_hash[:total_debt]) - ey_hash[:cash_and_equivalents]
+      ey = ey_hash[:ebit] / ev
+      net_fixed_assets = roc_hash[:total_assets] - roc_hash[:current_assets]
+      roc = roc_hash[:ebit] / (net_fixed_assets + roc_hash[:working_capital])
+
+      expect(completed_ey_hash[:enterprise_value]).to eq(ev)
+      expect(completed_ey_hash[:earnings_yield]).to eq(ey)
+      expect(completed_roc_hash[:return_on_capital]).to eq(roc)
+    end
+  end
+
   describe '.process_data_hash' do
     it 'processes the given data' do
       ticker = 'AAPL'
