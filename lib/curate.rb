a8d7f95cd4a84ca84f91fa6e95e10a895fd8ed98
mntj <mntj11@gmail.com>
2014-11-12 17:15:28 -0500

    Change curate to read from russell3000.txt, change num_to_keep to 50

--- lib/curate.rb
+++ lib/curate.rb
@@ -3,7 +3,7 @@ require 'benchmark'
 require_relative 'stock_reader'
 
 def curate(num_to_keep)
-  ticker_file   = File.open("russell20.txt", "r")
+  ticker_file   = File.open("russell3000.txt", "r")
   ticker_array  = StockReader.extract_tickers(ticker_file)
   processed_data = StockReader.combine_data(ticker_array)
   ey_data = StockReader.sort_company_data(processed_data, :earnings_yield, num_to_keep)
@@ -14,7 +14,7 @@ def curate(num_to_keep)
   StockReader.create_csv(roc_data, "Return on Capital #{Date.today}")
 end
 
-curate(20)
+curate(50)
 
 
 
--- lib/stock_reader.rb
+++ lib/stock_reader.rb
@@ -63,6 +63,7 @@ module StockReader
     ticker_array.map do |ticker|
       ey = get_ey_and_roc(ticker, :ey)
       roc = get_ey_and_roc(ticker, :roc)
+      p "Done with #{ticker}"
       ey.merge(roc) if ey && roc
     end
   end
