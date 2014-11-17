1e076b92a16e599652d1b61e8239f8d839072ed6
mntj <mntj11@gmail.com>
2014-11-17 17:27:02 -0500

    Passing test for TickerExtractor#extract_tickers

--- app/models/ticker_extractor.rb
+++ app/models/ticker_extractor.rb
@@ -1,11 +1,11 @@
 class TickerExtractor
-  # attr_reader :ticker_file
+  attr_reader :ticker_file
 
-  # def initialize(ticker_file)
-  #   @ticker_file = ticker_file
-  # end
+  def initialize(ticker_file)
+    @ticker_file = ticker_file
+  end
 
-  # def extract_tickers
-  #   File.readlines(ticker_file).map { |line| line.match(/\s(\w+)$/).to_s.strip }
-  # end
+  def extract_tickers
+    File.readlines(ticker_file).map { |line| line.match(/\s(\w+)$/).to_s.strip }
+  end
 end
