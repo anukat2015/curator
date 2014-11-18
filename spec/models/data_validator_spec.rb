05c106a0462df6d1555e9149c35015614350c8b5
mntj <mntj11@gmail.com>
2014-11-18 12:47:58 -0500

    Failing test for DataValidator#data_present?

--- spec/models/data_validator_spec.rb
+++ spec/models/data_validator_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DataValidator, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#data_present?' do
+    it 'returns true if data is present' do
+      actual = DataValidator.new('AAPL').data_present?
+      expect(actual).to be(true)
+    end
+  end
 end
