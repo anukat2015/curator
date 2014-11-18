d27d0d2d96014e156bcc857434acb335b5a16b7e
mntj <mntj11@gmail.com>
2014-11-18 13:00:41 -0500

    Passing test for non-existent tickers - DataValidator#data_present?

--- spec/models/data_validator_spec.rb
+++ spec/models/data_validator_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe DataValidator, :type => :model do
       actual = DataValidator.new('AAPL').data_present?
       expect(actual).to be(true)
     end
+
+    it 'returns false if data is not present' do
+      actual = DataValidator.new('NOTAREALTICKER').data_present?
+      expect(actual).to be(false)
+    end
   end
 end
