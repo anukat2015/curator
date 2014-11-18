45bc137c0ed002f7fbfd5d5addb2269425b4b363
mntj <mntj11@gmail.com>
2014-11-18 12:50:44 -0500

    Passing test for DataValidator#data_present?

--- app/models/data_validator.rb
+++ app/models/data_validator.rb
@@ -1,2 +1,12 @@
 class DataValidator
+  attr_reader :ticker
+
+  def initialize(ticker)
+    @ticker = ticker
+  end
+
+  def data_present?
+    response = HTTParty.get("https://www.quandl.com/api/v1/datasets/SF1/#{ticker}_EBIT_MRQ.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}")
+    response.size > 1
+  end
 end
