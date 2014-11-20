8f24e8983c4461e36a4419af9d191f66d4bd588b
mntj <mntj11@gmail.com>
2014-11-20 18:03:16 -0500

    Failing test for DataValidator#data_received?

--- spec/models/data_validator_spec.rb
+++ spec/models/data_validator_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe DataValidator, :type => :model do
       expect(actual).to be(false)
     end
   end
+
+  describe '#data_received?' do
+    it 'returns true if data is received' do
+      sample_data = [{"data"=>[["2014-09-27", 35295000000.0]]}]
+      actual = DataValidator.new.data_received?(sample_data.map { |k,v| v })
+      expect(actual).to be(true)
+    end
+  end
 end
