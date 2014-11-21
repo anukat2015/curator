eb51c29d53c311dfbc40ef5947f7ee62204b2ff3
mntj <mntj11@gmail.com>
2014-11-21 16:03:33 -0500

    Extract out test_url for DataValidator

--- app/models/data_validator.rb
+++ app/models/data_validator.rb
@@ -1,17 +1,25 @@
 class DataValidator
   attr_reader :ticker, :responses
 
+  BASE_URL = "https://www.quandl.com/api/v1/datasets/SF1/"
+
   def initialize(options = {})
     @ticker = options[:ticker]
     @responses = options[:responses]
   end
 
   def data_present?
-    response = HTTParty.get("https://www.quandl.com/api/v1/datasets/SF1/#{ticker}_EBIT_MRQ.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}")
+    response = HTTParty.get(test_url)
     response.size > 1
   end
 
   def data_received?
     responses.map { |res| res["data"] }.none? { |data| data.to_a.empty? }
+  end
+
+  private
+
+  def test_url
+    BASE_URL + "#{ticker}_EBIT_MRQ.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}"
   end
 end
