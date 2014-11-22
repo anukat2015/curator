1a4ed03076d65a278871826dfd3ad311e7aee344
mntj <mntj11@gmail.com>
2014-11-22 14:29:47 -0500

    Remove old files. Finish refactor of StockReader module into classes

--- lib/curate.rb
+++ lib/curate.rb
@@ -1,19 +1,6 @@
 require 'date'
-require_relative 'stock_reader'
 
 def curate(num_to_keep)
-  ticker_file   = File.open("russell20.txt", "r")
-  ticker_array  = StockReader.extract_tickers(ticker_file)
-  processed_data = StockReader.combine_data(ticker_array)
-  ey_data = StockReader.sort_company_data(processed_data, :earnings_yield, num_to_keep)
-  roc_data = StockReader.sort_company_data(processed_data, :return_on_capital, num_to_keep)
-  StockReader.create_company_reports(ey_data, CompanyReportByEarningsYield)
-  StockReader.create_company_reports(roc_data, CompanyReportByReturnOnCapital)
-  StockReader.create_csv(ey_data, "Earnings Yield #{Date.today}")
-  StockReader.create_csv(roc_data, "Return on Capital #{Date.today}")
-end
-
-def curate_new(num_to_keep)
   ticker_file = File.open("russell20.txt", "r")
   ticker_array = TickerExtractor.new(ticker_file: ticker_file).extract_tickers
   company_data = DataFactory.new(ticker_array: ticker_array).make_company_hashes
@@ -25,8 +12,7 @@ def curate_new(num_to_keep)
   Clerk.new(data: roc_data, file_name: "Return on Capital #{Date.today}").create_csv
 end
 
-#curate(20)
-curate_new(20)
+curate(20)
 
 
 
