74ab108b11cf1e83e59e4dcc5ecd9822aaa8241d
mntj <mntj11@gmail.com>
2014-11-22 18:30:28 -0500

    Add passing tests for Custodian#curate

--- spec/models/custodian_spec.rb
+++ spec/models/custodian_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe Custodian, :type => :model do
     before(:all) do
       CompanyReportByEarningsYield.delete_all
       CompanyReportByReturnOnCapital.delete_all
+
+      Custodian.new(file: "russell5.txt", num_to_keep: 3).curate
     end
 
     it 'creates reports' do
-      Custodian.new(file: "russell5.txt", num_to_keep: 3).curate
       expect(CompanyReportByEarningsYield.count).to eq(3)
       expect(CompanyReportByReturnOnCapital.count).to eq(3)
     end
@@ -24,9 +25,11 @@ RSpec.describe Custodian, :type => :model do
     it 'creates correct CSV files' do
       arr = CSV.read("Earnings Yield #{Date.today}.csv")
       expect(arr[0]).to eq(csv_columns)
+      expect(arr[1]).to eq(csv_data_TSRA)
 
       arr = CSV.read("Return on Capital #{Date.today}.csv")
       expect(arr[0]).to eq(csv_columns)
+      expect(arr[3]).to eq(csv_data_TTI)
     end
 
     after(:all) do
