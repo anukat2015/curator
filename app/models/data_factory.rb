e9edc935c09138e8ac4cf05a8a490dfdb3b08403
mntj <mntj11@gmail.com>
2014-11-22 13:22:05 -0500

    Failing test for DataFactory#make_company_hashes

--- app/models/data_factory.rb
+++ app/models/data_factory.rb
@@ -1,2 +1,10 @@
-class DataFactory < ActiveRecord::Base
+class DataFactory
+  attr_reader :ticker_array
+
+  def initialize(ticker_array:)
+    @ticker_array = ticker_array
+  end
+
+  def make_company_hashes
+  end
 end
--- spec/models/data_factory_spec.rb
+++ spec/models/data_factory_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'sample_data'
 
 RSpec.describe DataFactory, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#make_company_hashes' do
+    it 'returns an array of combined hashes' do
+      ticker_array = ['AAPL']
+      actual = DataFactory.new(ticker_array: ticker_array).make_company_hashes
+      expected = [return_on_capital_data_1.merge(earnings_yield_data_1)]
+      expect(actual).to eq(expected)
+    end
+  end
 end
