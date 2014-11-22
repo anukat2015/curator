a36e15f382b22bd86ebc92fda94f075fd3ec7661
mntj <mntj11@gmail.com>
2014-11-22 13:04:25 -0500

    DataWorker#get_ey_and_roc passing

--- app/models/data_worker.rb
+++ app/models/data_worker.rb
@@ -1,2 +1,22 @@
 class DataWorker
+  attr_reader :ticker, :type
+
+  def initialize(ticker:, type:)
+    @ticker = ticker
+    @type = type
+  end
+
+  def get_ey_and_roc
+    if DataValidator.new(ticker: ticker).data_present?
+      if type == :ey
+        query_hash = {:ebit => "EBIT_MRQ", :market_cap => "MARKETCAP", :cash_and_equivalents => "CASHNEQ_MRQ", :total_debt => "DEBT_MRQ"}
+      elsif type == :roc
+        query_hash = {:ebit => "EBIT_MRQ", :total_assets => "ASSETS_MRQ", :current_assets => "ASSETSC_MRQ", :working_capital => "WORKINGCAPITAL_MRQ"}
+      end
+      data = DataFetcher.new(ticker: ticker, query_hash: query_hash).get_company_data
+    end
+    if data && DataValidator.new(responses: data.map { |k,v| v }).data_received?
+      HashBuilder.new(ticker: ticker, data: data).build_hash
+    end
+  end
 end
--- spec/models/data_worker_spec.rb
+++ spec/models/data_worker_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe DataWorker, :type => :model do
   describe '#get_ey_and_roc' do
     it 'amalgamates data correctly' do
       ticker = 'AAPL'
-      ey_data = DataWorker.new(ticker, :ey).get_ey_and_roc
-      roc_data =  DataWorker.new(ticker, :roc).get_ey_and_roc
+      ey_data = DataWorker.new(ticker: ticker, type: :ey).get_ey_and_roc
+      roc_data =  DataWorker.new(ticker: ticker, type: :roc).get_ey_and_roc
 
       expect(ey_data).to eq(earnings_yield_data_1)
       expect(roc_data).to eq(return_on_capital_data_1)
