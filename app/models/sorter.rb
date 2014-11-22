63294007d96ec0dac0626da8bf7cf17311bc6ef4
mntj <mntj11@gmail.com>
2014-11-22 13:37:02 -0500

    Failing test for Sorter#sort_company_data

--- app/models/sorter.rb
+++ app/models/sorter.rb
@@ -1,2 +1,12 @@
 class Sorter
+  attr_reader :data, :metric, :num_to_keep
+
+  def initialize(data:, metric:, num_to_keep:)
+    @data = data
+    @metric = metric
+    @num_to_keep = num_to_keep
+  end
+
+  def sort_company_data
+  end
 end
--- spec/models/sorter_spec.rb
+++ spec/models/sorter_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'sample_data'
 
 RSpec.describe Sorter, :type => :model do
-
+  describe '#sort_company_data' do
+    it 'sorts by the given metric' do
+      company_data = [earnings_yield_data_1, earnings_yield_data_2]
+      actual = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: 30).sort_company_data
+      expect(actual).to eq(company_data.reverse)
+    end
+  end
 end
