2987aea77ef7149200c5456d33c8e8157a3f7823
mntj <mntj11@gmail.com>
2014-11-22 14:25:18 -0500

    Correct new curate method

--- lib/curate.rb
+++ lib/curate.rb
@@ -17,10 +17,10 @@ def curate_new(num_to_keep)
   ticker_file = File.open("russell20.txt", "r")
   ticker_array = TickerExtractor.new(ticker_file: ticker_file).extract_tickers
   company_data = DataFactory.new(ticker_array: ticker_array).make_company_hashes
-  ey_data = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: num_to_keep)
-  roc_data = Sorter.new(data: company_data, metric: :return_on_capital, num_to_keep: num_to_keep)
-  ReportCreator.new(data: ey_data, type: CompanyReportByEarningsYield)
-  ReportCreator.new(data: roc_data, type: CompanyReportByReturnOnCapital)
+  ey_data = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: num_to_keep).sort_company_data
+  roc_data = Sorter.new(data: company_data, metric: :return_on_capital, num_to_keep: num_to_keep).sort_company_data
+  ReportCreator.new(data: ey_data, type: CompanyReportByEarningsYield).create_company_reports
+  ReportCreator.new(data: roc_data, type: CompanyReportByReturnOnCapital).create_company_reports
   Clerk.new(data: ey_data, file_name: "Earnings Yield #{Date.today}").create_csv
   Clerk.new(data: roc_data, file_name: "Return on Capital #{Date.today}").create_csv
 end
