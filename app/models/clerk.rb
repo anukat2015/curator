451abc552f94a38391dace21306403caeaecc631
mntj <mntj11@gmail.com>
2014-11-22 13:59:37 -0500

    Clerk#create_csv passing

--- app/models/clerk.rb
+++ app/models/clerk.rb
@@ -1,2 +1,19 @@
+require 'csv'
+
 class Clerk
+  attr_reader :data, :file_name
+
+  def initialize(data:, file_name:)
+    @data = data
+    @file_name = file_name
+  end
+
+  def create_csv
+    CSV.open("#{file_name}.csv", "wb") do |csv|
+      csv << data.first.map { |k, v| k.to_s.capitalize.gsub("_", " ") }
+      data.each do |data|
+        csv << data.each_value.map { |val| val.to_s }
+      end
+    end
+  end
 end
--- spec/models/clerk_spec.rb
+++ spec/models/clerk_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Clerk, :type => :model do
   describe '#create_csv' do
     it 'creates csv files' do
       return_on_capital_array = [return_on_capital_data_1, return_on_capital_data_2]
-      Clerk.new(return_on_capital_array, "company_report").create_csv
+      Clerk.new(data: return_on_capital_array, file_name: "company_report").create_csv
       expect(File.file?("company_report.csv")).to be(true)
     end
     after(:all) do
