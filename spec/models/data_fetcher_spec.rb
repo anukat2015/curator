1d75c3cee1955f1d889a35406a38944482e3e7ea
mntj <mntj11@gmail.com>
2014-11-21 19:27:54 -0500

    Passing test for DataFetcher#get_company_data for return on capital

--- spec/models/data_fetcher_spec.rb
+++ spec/models/data_fetcher_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe DataFetcher, :type => :model do
       expect(ey_data[:cash]["data"].flatten[1]).to be_kind_of(Float)
       expect(ey_data[:debt]["data"].flatten[1]).to be_kind_of(Float)
     end
+
+    it 'retrieves data for calculating return on capital' do
+      ticker = 'AAPL'
+      roc_hash = {:total_assets => "ASSETS_MRQ", :current_assets => "ASSETSC_MRQ", :working_capital => "WORKINGCAPITAL_MRQ"}
+      roc_data = DataFetcher.new(ticker: ticker, query_hash: roc_hash).get_company_data
+      expect(roc_data[:total_assets]["data"].flatten[1]).to be_kind_of(Float)
+      expect(roc_data[:current_assets]["data"].flatten[1]).to be_kind_of(Float)
+      expect(roc_data[:working_capital]["data"].flatten[1]).to be_kind_of(Float)
+    end
   end
 end
