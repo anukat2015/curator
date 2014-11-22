8d0cc21ec1f19a939b3eab5011704343487b569b
mntj <mntj11@gmail.com>
2014-11-22 13:56:27 -0500

    Failing test for Clerk#create_csv

--- spec/models/clerk_spec.rb
+++ spec/models/clerk_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require 'sample_data'
 
 RSpec.describe Clerk, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create_csv' do
+    it 'creates csv files' do
+      return_on_capital_array = [return_on_capital_data_1, return_on_capital_data_2]
+      Clerk.new(return_on_capital_array, "company_report").create_csv
+      expect(File.file?("company_report.csv")).to be(true)
+    end
+    after(:all) do
+      File.delete("company_report.csv")
+    end
+  end
 end
