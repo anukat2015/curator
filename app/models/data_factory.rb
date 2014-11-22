82cdf28dab4e4320ee2bbc64aa0fc03ce4f9872b
mntj <mntj11@gmail.com>
2014-11-22 13:25:49 -0500

    DataFactory#make_company_hashes passing

--- app/models/data_factory.rb
+++ app/models/data_factory.rb
@@ -6,5 +6,11 @@ class DataFactory
   end
 
   def make_company_hashes
+    ticker_array.map do |ticker|
+      ey = DataWorker.new(ticker: ticker, type: :ey).get_ey_and_roc
+      roc = DataWorker.new(ticker: ticker, type: :roc).get_ey_and_roc
+      puts "Done with #{ticker} No. #{ticker_array.index(ticker)}"
+      ey.merge(roc) if ey && roc
+    end
   end
 end
