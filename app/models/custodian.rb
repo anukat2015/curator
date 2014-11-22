81ffc7e46a568795f35cba6fa2cd61a47c5e0e9a
mntj <mntj11@gmail.com>
2014-11-22 17:40:40 -0500

    Custodian#curate tests passing

--- app/models/custodian.rb
+++ app/models/custodian.rb
@@ -1,2 +1,22 @@
+require 'date'
+
 class Custodian
+  attr_reader :file, :num_to_keep
+
+  def initialize(file:, num_to_keep:)
+    @file = file
+    @num_to_keep = num_to_keep
+  end
+
+  def curate
+    ticker_file = File.open(file, "r")
+    ticker_array = TickerExtractor.new(ticker_file: ticker_file).extract_tickers
+    company_data = DataFactory.new(ticker_array: ticker_array).make_company_hashes
+    ey_data = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: num_to_keep).sort_company_data
+    roc_data = Sorter.new(data: company_data, metric: :return_on_capital, num_to_keep: num_to_keep).sort_company_data
+    ReportCreator.new(data: ey_data, type: CompanyReportByEarningsYield).create_company_reports
+    ReportCreator.new(data: roc_data, type: CompanyReportByReturnOnCapital).create_company_reports
+    Clerk.new(data: ey_data, file_name: "Earnings Yield #{Date.today}").create_csv
+    Clerk.new(data: roc_data, file_name: "Return on Capital #{Date.today}").create_csv
+  end
 end
--- spec/models/custodian_spec.rb
+++ spec/models/custodian_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Custodian, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#curate' do
+    before(:all) do
+      CompanyReportByEarningsYield.delete_all
+      CompanyReportByReturnOnCapital.delete_all
+    end
+
+    it 'creates reports' do
+      Custodian.new(file: "russell5.txt", num_to_keep: 3).curate
+      expect(CompanyReportByEarningsYield.count).to eq(3)
+      expect(CompanyReportByReturnOnCapital.count).to eq(3)
+    end
+
+    it 'creates CSV files' do
+      expect(File.file?("Earnings Yield #{Date.today}.csv")).to be(true)
+      expect(File.file?("Return on Capital #{Date.today}.csv")).to be(true)
+    end
+
+    it 'creates correct CSV files' do
+
+    end
+
+    # after(:all) do
+    #   File.delete("Earnings Yield #{Date.today}")
+    #   File.delete("Return on Capital #{Date.today}")
+    # end
+  end
 end
