625e6242c1da2a6f31593ccc0060214de057192f
mntj <mntj11@gmail.com>
2014-11-22 13:39:40 -0500

    Sorter#sort_company_data test passing

--- app/models/sorter.rb
+++ app/models/sorter.rb
@@ -8,5 +8,7 @@ class Sorter
   end
 
   def sort_company_data
+    data.reject! { |company| company.nil? || company[metric].nil? || company[metric].nan? }
+    data.sort_by { |company| company[metric].to_f }.reverse.take(num_to_keep)
   end
 end
