e8cbce52f551ffb8e0353fb3aad229416dc4db61
mntj <mntj11@gmail.com>
2014-11-22 13:48:41 -0500

    ReportCreator#create_company_reports passing

--- app/models/report_creator.rb
+++ app/models/report_creator.rb
@@ -1,2 +1,14 @@
 class ReportCreator
+  attr_reader :data, :type
+
+  def initialize(data:, type:)
+    @data = data
+    @type = type
+  end
+
+  def create_company_reports
+    data.each do |company|
+      type.create(company.each { |k,v| company[k] = v.to_s })
+    end
+  end
 end
