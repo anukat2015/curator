42002f8a908eeaab01e250f96e29f5a6edccc8ea
mntj <mntj11@gmail.com>
2014-12-03 16:31:46 -0500

    Abstract out base url in DataFetcher

--- app/models/data_fetcher.rb
+++ app/models/data_fetcher.rb
@@ -1,6 +1,8 @@
 class DataFetcher
   attr_reader :ticker, :query_hash
 
+  BASE_URL = "https://www.quandl.com/api/v1/datasets/SF1/"
+
   def initialize(ticker:, query_hash:)
     @ticker = ticker
     @query_hash = query_hash
@@ -9,7 +11,7 @@ class DataFetcher
   def get_company_data
     data_responses = {}
     query_hash.each do |metric, quandl_query|
-      data_responses[metric] = HTTParty.get("https://www.quandl.com/api/v1/datasets/SF1/#{ticker}_#{quandl_query}.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}")
+      data_responses[metric] = HTTParty.get(BASE_URL + "#{ticker}_#{quandl_query}.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}")
     end
     data_responses
   end
