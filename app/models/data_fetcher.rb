254f0c36f3e1791e25a04a611bb072311329c813
mntj <mntj11@gmail.com>
2015-01-06 14:20:31 -0500

    Remove extra sleeps

--- app/models/data_fetcher.rb
+++ app/models/data_fetcher.rb
@@ -15,8 +15,9 @@ class DataFetcher
     query_hash.each do |metric, quandl_query|
       data_responses[metric] = HTTParty.get(BASE_URL + "#{ticker}_#{quandl_query}.json?rows=1&auth_token=#{ENV['QUANDL_AUTH_TOKEN']}")
       ErrorChecker.check_for_errors(data_responses[metric], ticker)
-      # Slow down request rate to avoid speed throttling by Quandl
-      sleep 3
+
+      sleep 2 # Slow down request rate to avoid speed throttling by Quandl
+
     end
     data_responses
   end
