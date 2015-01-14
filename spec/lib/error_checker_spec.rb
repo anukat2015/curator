504774c50364b5bdd96dee4bd7b4b2a911c3980f
mntj <mntj11@gmail.com>
2015-01-14 15:34:42 -0500

    Setup ErrorChecker spec format

---
+++ spec/lib/error_checker_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'sample_data'
+
+RSpec.describe ErrorChecker do
+  describe '.check_for_errors' do
+    it 'warns about errors' do
+      expect { ErrorChecker.check_for_errors() }.to output().to_stderr
+    end
+  end
+end
