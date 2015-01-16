d356cf1e8d301e4aa9f783497ceb84c2d1bc5e41
mntj <mntj11@gmail.com>
2015-01-16 18:43:10 -0500

    Add ErrorChecker.check_for_errors spec for warning about non Hash responses

--- spec/lib/error_checker_spec.rb
+++ spec/lib/error_checker_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe ErrorChecker do
     it 'warns response[\'error\']' do
       expect { ErrorChecker.check_for_errors(error_response, 'AAFL') }.to output("AAFL ----- Requested entity does not exist.\n").to_stderr
     end
+
+    # it 'warns response[\'errors\']' do
+    # end
+
+    it 'warns non Hash objects' do
+      expect { ErrorChecker.check_for_errors('Not a response', 'NOTATICKER') }.to output("Not a response\n").to_stderr
+    end
   end
 end
