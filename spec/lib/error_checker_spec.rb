787fc255f0c9345a3306ff29c9e2688e661bede0
mntj <mntj11@gmail.com>
2015-01-16 18:35:14 -0500

    Add ErrorChecker.check_for_errors warn spec for response[error]

--- spec/lib/error_checker_spec.rb
+++ spec/lib/error_checker_spec.rb
@@ -4,7 +4,7 @@ require 'sample_data'
 RSpec.describe ErrorChecker do
   describe '.check_for_errors' do
     it 'warns about errors' do
-      #expect { ErrorChecker.check_for_errors() }.to output().to_stderr
+      expect { ErrorChecker.check_for_errors(error_response, 'AAFL') }.to output("AAFL ----- Requested entity does not exist.\n").to_stderr
     end
   end
 end
