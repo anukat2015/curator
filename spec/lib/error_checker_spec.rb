88fbad540f16e762ec4d87ef9b41dd9e6723515a
mntj <mntj11@gmail.com>
2015-01-16 18:37:07 -0500

    coverage and spec files

--- spec/lib/error_checker_spec.rb
+++ spec/lib/error_checker_spec.rb
@@ -3,7 +3,7 @@ require 'sample_data'
 
 RSpec.describe ErrorChecker do
   describe '.check_for_errors' do
-    it 'warns about errors' do
+    it 'warns response[\'error\']' do
       expect { ErrorChecker.check_for_errors(error_response, 'AAFL') }.to output("AAFL ----- Requested entity does not exist.\n").to_stderr
     end
   end
