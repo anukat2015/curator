af572ec9ab9b70d0d088a4e9f615d62398cb92e7
mntj <mntj11@gmail.com>
2015-01-17 10:14:30 -0500

    Add slow_down private method to ErrorChecker

--- lib/error_checker.rb
+++ lib/error_checker.rb
@@ -6,6 +6,16 @@ module ErrorChecker
       warn "#{ticker} ----- " + response['errors']
     elsif !response.is_a?(Hash)
       warn response.to_s
+        slow_down if response.include? "too quickly"
     end
+  end
+
+  private
+
+  def slow_down
+    time_out = 30
+    puts "Pausing for #{time_out} seconds..."
+    sleep time_out
+    puts "Resuming..."
   end
 end
