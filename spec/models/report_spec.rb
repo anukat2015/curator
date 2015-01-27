9fc9763e3134a78e05dc5b50290a9bb00d9954ea
Corey Leveen <mntj11@gmail.com>
2015-01-26 23:25:14 -0500

    Expand on failing spec for Report attribute acceptance

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -6,8 +6,33 @@ RSpec.describe Report, :type => :model do
     expect(report).to be_an_instance_of(Report)
   end
 
-  it 'accepts a name attribute' do
-    report = Report.create!(name: 'Acme Corporation')
-    expect(report.name).to equal('Acme Corporation')
+  it 'accepts the necessary attributes' do
+    report = Report.create!(
+      name: 'Acme Corporation',
+      symbol: 'ACME',
+      ebit: 123456789.101,
+      ebit_date: '2000-01-01',
+      market_cap: 123456789.101,
+      market_cap_date: '2000-01-01',
+      working_capital: 123456789.101,
+      working_capital_date: '2000-01-01',
+      fixed_assets: 123456789.101,
+      fixed_assets_date: '2000-01-01',
+      total_assets: 123456789.101,
+      total_assets_date: '2000-01-01',
+      current_assets: 123456789.101,
+      current_assets_date: '2000-01-01',
+      total_debt: 123456789.101,
+      total_debt_date: '2000-01-01',
+      cash_and_equivalents: 123456789.101,
+      cash_and_equivalents_date: '2000-01-01',
+      enterprise_value: 123456789.101,
+      return_on_capital: 0.2,
+      earnings_yield: 0.2
+      )
+  end
+
+  after(:all) do
+    Report.delete_all
   end
 end
