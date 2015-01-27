49115f6748c64d23289df5735cbc130a80fca888
Corey Leveen <mntj11@gmail.com>
2015-01-26 22:28:57 -0500

    Add passing spec for Report creation. Add failing spec for Report name attribute acceptance

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Report, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be created' do
+    report = Report.create!
+    expect(report).to be_an_instance_of(Report)
+  end
+
+  it 'accepts a name attribute' do
+    report = Report.create!(name: 'Acme Corporation')
+    expect(report.name).to equal('Acme Corporation')
+  end
 end
