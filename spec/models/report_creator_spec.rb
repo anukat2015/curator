bce3c506702e423c70fc0b4c001bbdc61cfef627
Corey Leveen <mntj11@gmail.com>
2015-01-27 21:31:02 -0500

    Adapt report_creator_spec for using Reports only

--- spec/models/report_creator_spec.rb
+++ spec/models/report_creator_spec.rb
@@ -4,20 +4,13 @@ require 'sample_data'
 RSpec.describe ReportCreator do
   describe '#create_company_reports' do
     before(:all) do
-      CompanyReportByEarningsYield.delete_all
-      CompanyReportByReturnOnCapital.delete_all
+      Report.delete_all
     end
 
-    it 'creates earnings yield reports' do
+    it 'creates reports' do
       earnings_yield_array = [earnings_yield_data_1, earnings_yield_data_2]
-      ReportCreator.new(data: earnings_yield_array, type: CompanyReportByEarningsYield).create_company_reports
-      expect(CompanyReportByEarningsYield.count).to eq(2)
-    end
-
-    it 'creates return on capital reports' do
-      return_on_capital_array = [return_on_capital_data_1, return_on_capital_data_2]
-      ReportCreator.new(data: return_on_capital_array, type: CompanyReportByReturnOnCapital).create_company_reports
-      expect(CompanyReportByReturnOnCapital.count).to eq(2)
+      ReportCreator.new(data: earnings_yield_array, type: Report).create_company_reports
+      expect(Report.count).to eq(2)
     end
   end
 end
