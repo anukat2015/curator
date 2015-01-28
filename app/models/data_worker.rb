2502f2edc1597ade5e78fe7a4d51ef17ee555efc
Corey Leveen <mntj11@gmail.com>
2015-01-27 22:13:28 -0500

    Begin extracting methods in DataWorker

--- app/models/data_worker.rb
+++ app/models/data_worker.rb
@@ -1,16 +1,14 @@
 class DataWorker
   def get_ey_and_roc
-    if DataValidator.new(ticker: ticker).data_present?
+    if data_exists?
       if type == :ey
-        query_hash = {:ebit => "EBIT_MRQ", :market_cap => "MARKETCAP", :cash_and_equivalents => "CASHNEQ_MRQ", :total_debt => "DEBT_MRQ"}
+        query_hash = earnings_yield_query_hash
       elsif type == :roc
-        query_hash = {:ebit => "EBIT_MRQ", :total_assets => "ASSETS_MRQ", :current_assets => "ASSETSC_MRQ", :working_capital => "WORKINGCAPITAL_MRQ"}
+        query_hash = return_on_capital_query_hash
       end
-      data = DataFetcher.new(ticker: ticker, query_hash: query_hash).get_company_data
+      data = fetch_data(query_hash)
     end
-    if data && DataValidator.new(responses: data.map { |k,v| v }).data_received?
-      HashBuilder.new(ticker: ticker, data: data).build_hash
-    end
+    build_hash(data) if data_retrieved?(data)
   end
 
   private
@@ -20,5 +18,29 @@ class DataWorker
   def initialize(ticker:, type:)
     @ticker = ticker
     @type = type
+  end
+
+  def data_exists?
+    DataValidator.new(ticker: ticker).data_present?
+  end
+
+  def earnings_yield_query_hash
+    {:ebit => "EBIT_MRQ", :market_cap => "MARKETCAP", :cash_and_equivalents => "CASHNEQ_MRQ", :total_debt => "DEBT_MRQ"}
+  end
+
+  def return_on_capital_query_hash
+    {:ebit => "EBIT_MRQ", :total_assets => "ASSETS_MRQ", :current_assets => "ASSETSC_MRQ", :working_capital => "WORKINGCAPITAL_MRQ"}
+  end
+
+  def data_retrieved?(data)
+    data && DataValidator.new(responses: data.map { |k,v| v }).data_received?
+  end
+
+  def fetch_data(queries)
+    DataFetcher.new(ticker: ticker, query_hash: queries).get_company_data
+  end
+
+  def build_hash(data)
+    HashBuilder.new(ticker: ticker, data: data).build_hash
   end
 end
