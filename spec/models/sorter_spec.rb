edd1cda2d78f0803075a5a4de9dd5391f80f7000
Corey Leveen <mntj11@gmail.com>
2015-02-08 21:50:30 -0500

    Add spec to test that Sorter#sort_company_data keeps the specified number of companies

--- spec/models/sorter_spec.rb
+++ spec/models/sorter_spec.rb
@@ -20,5 +20,14 @@ RSpec.describe Sorter do
       expected = [sample_company_data_2, sample_company_data_3, sample_company_data_1]
       expect(actual).to eq(expected)
     end
+
+    it 'keeps the specified number of companies' do
+      company_data = [sample_company_data_1, sample_company_data_3, sample_company_data_2]
+
+      actual = Sorter.new(data: company_data, metric: :return_on_capital, num_to_keep: 2).sort_company_data
+
+      expected = [sample_company_data_2, sample_company_data_3]
+      expect(actual).to eq(expected)
+    end
   end
 end
