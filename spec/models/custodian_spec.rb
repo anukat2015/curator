d6ef1efb47c0fa3e72df453ff432ec6cfe4dc94a
Corey Leveen <mntj11@gmail.com>
2015-02-08 22:39:18 -0500

    Remove CSV creation tests from Custodian#curate spec

--- spec/models/custodian_spec.rb
+++ spec/models/custodian_spec.rb
@@ -16,25 +16,5 @@ RSpec.describe Custodian do
     it 'creates reports' do
       expect(Report.count).to eq(4)
     end
-
-    it 'creates CSV files' do
-      expect(File.file?("Earnings Yield #{Date.today}.csv")).to be(true)
-      expect(File.file?("Return on Capital #{Date.today}.csv")).to be(true)
-    end
-
-    it 'creates correct CSV files' do
-      arr = CSV.read("Earnings Yield #{Date.today}.csv")
-      expect(arr[0]).to eq(csv_columns)
-      expect(arr[1]).to eq(csv_data_TSRA)
-
-      arr = CSV.read("Return on Capital #{Date.today}.csv")
-      expect(arr[0]).to eq(csv_columns)
-      expect(arr[4]).to eq(csv_data_TTI)
-    end
-
-    after(:all) do
-      File.delete("Earnings Yield #{Date.today}.csv")
-      File.delete("Return on Capital #{Date.today}.csv")
-    end
   end
 end
