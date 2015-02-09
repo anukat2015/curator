123a48f392c323dc05230aaf71b9c6604245e263
Corey Leveen <mntj11@gmail.com>
2015-02-08 21:45:19 -0500

    Add return on capital sorting spec to Sorter#sort_company_data

--- spec/models/sorter_spec.rb
+++ spec/models/sorter_spec.rb
@@ -6,7 +6,16 @@ RSpec.describe Sorter do
     it 'sorts correctly by earnings yield' do
       company_data = [sample_company_data_1, sample_company_data_3, sample_company_data_2]
 
-      actual = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: 30).sort_company_data
+      actual = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: 3).sort_company_data
+
+      expected = [sample_company_data_2, sample_company_data_3, sample_company_data_1]
+      expect(actual).to eq(expected)
+    end
+
+    it 'sorts correctly by return on capital' do
+      company_data = [sample_company_data_1, sample_company_data_3, sample_company_data_2]
+
+      actual = Sorter.new(data: company_data, metric: :return_on_capital, num_to_keep: 3).sort_company_data
 
       expected = [sample_company_data_2, sample_company_data_3, sample_company_data_1]
       expect(actual).to eq(expected)
