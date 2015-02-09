cf82dc6fea3f4fb8c9f99c41fca8e80de14209bf
Corey Leveen <mntj11@gmail.com>
2015-02-08 21:28:05 -0500

    Change Sorter#sort_company_data spec to accept new sample company data

--- spec/models/sorter_spec.rb
+++ spec/models/sorter_spec.rb
@@ -3,10 +3,13 @@ require 'sample_data'
 
 RSpec.describe Sorter do
   describe '#sort_company_data' do
-    it 'sorts by the given metric' do
-      company_data = [earnings_yield_data_1, earnings_yield_data_2]
+    it 'sorts correctly by earnings yield' do
+      company_data = [sample_company_data_1, sample_company_data_3, sample_company_data_2]
+
       actual = Sorter.new(data: company_data, metric: :earnings_yield, num_to_keep: 30).sort_company_data
-      expect(actual).to eq(company_data)
+
+      expected = [sample_company_data_2, sample_company_data_3, sample_company_data_1]
+      expect(actual).to eq(expected)
     end
   end
 end
