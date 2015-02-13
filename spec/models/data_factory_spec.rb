28b5bd0bbbe41ce35839d8b8ba771680a1b25d81
Corey Leveen <mntj11@gmail.com>
2015-02-12 23:30:52 -0500

    Failing spec for DataFactory#make_company_hashes

--- spec/models/data_factory_spec.rb
+++ spec/models/data_factory_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe DataFactory do
     it 'returns an array of combined hashes' do
       ticker_array = ['AAPL']
       actual = DataFactory.new(ticker_array: ticker_array).make_company_hashes
-      expected = [return_on_capital_data_1.merge(earnings_yield_data_1)]
+      expected = [sample_company_data_1]
       expect(actual).to eq(expected)
     end
   end
