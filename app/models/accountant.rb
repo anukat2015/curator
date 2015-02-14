4cbf0906bbe9cd244d198b2cb98b9462e9106897
Corey Leveen <mntj11@gmail.com>
2015-02-13 21:05:49 -0500

    Accountant#calculate_ey_and_roc assigns fixed_assets to data hash object

--- app/models/accountant.rb
+++ app/models/accountant.rb
@@ -4,8 +4,8 @@ class Accountant
       data[:enterprise_value] = (data[:market_cap] + data[:total_debt]) - data[:cash_and_equivalents]
       data[:earnings_yield] = data[:ebit] / data[:enterprise_value]
     elsif data[:total_assets] && data[:current_assets]
-      net_fixed_assets = data[:total_assets] - data[:current_assets]
-      data[:return_on_capital] = data[:ebit] / (net_fixed_assets + data[:working_capital])
+      data[:fixed_assets] = data[:total_assets] - data[:current_assets]
+      data[:return_on_capital] = data[:ebit] / (data[:fixed_assets] + data[:working_capital])
     end
     data
   end
