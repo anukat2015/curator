8ed2a7ad55614e64a4c97afc2cbb38cb09406989
Corey Leveen <mntj11@gmail.com>
2015-02-14 23:25:16 -0500

    Implement spec for Overseer#retrieve_and_persist_data

--- app/models/overseer.rb
+++ app/models/overseer.rb
@@ -1,2 +1,22 @@
 class Overseer
+  def retrieve_and_persist_data
+    get_company_data
+    persist_company_data
+  end
+
+  private
+
+  def initialize(ticker_file:)
+    @ticker_file = ticker_file
+  end
+
+  attr_reader :ticker_file, :company_data
+
+  def get_company_data
+    @company_data = DataGatherer.new(ticker_file: ticker_file).gather_data
+  end
+
+  def persist_company_data
+    ReportCreator.new(data: company_data).create_company_reports
+  end
 end
