c4eaea576db068925592e4f1f8cf9a68124171bd
Corey Leveen <mntj11@gmail.com>
2015-02-27 20:49:33 -0500

    Extract message into MessageMaker class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,8 @@ class WelcomeController < ApplicationController
       @reports = Report.select(:symbol, :return_on_capital, :earnings_yield)
                        .order(report_params[:sort_by] => :desc)
                        .limit(report_params[:limit].to_i)
-      make_message(report_params[:sort_by], report_params[:limit])
+
+      @message = MessageMaker.new(report_params).make_message
     end
   end
 
@@ -12,23 +13,5 @@ class WelcomeController < ApplicationController
 
   def report_params
     params.permit(:sort_by, :limit)
-  end
-
-  def make_message(sort_by, limit)
-    @message = "Sorting by "
-
-    if sort_by == "return_on_capital"
-      @message << "Return on Capital"
-    elsif sort_by == "earnings_yield"
-      @message << "Earnings Yield"
-    else
-      @message << "nothing"
-    end
-
-    if limit == "2000"
-      @message << "\nLimit: None"
-    else
-      @message << "\nLimit: #{report_params[:limit]}"
-    end
   end
 end
