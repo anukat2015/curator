85e2ab8c38d612e19337fe4b0abdb98f773019db
Corey Leveen <mntj11@gmail.com>
2015-02-28 14:08:10 -0500

    Add param specs to WelcomeController

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,8 +2,11 @@ require "rails_helper"
 
 RSpec.describe WelcomeController, :type => :controller do
   describe 'GET #index' do
-    before(:each) do
+    before(:all) do
       Report.delete_all
+
+      Report.create!(symbol: 'AAPL', return_on_capital: 0.12, earnings_yield: 0.21)
+      Report.create!(symbol: 'TSRA', return_on_capital: 0.21, earnings_yield: 0.12)
     end
 
     it 'responds successfully with an HTTP 200 status code' do
@@ -17,12 +20,23 @@ RSpec.describe WelcomeController, :type => :controller do
       expect(response).to render_template("index")
     end
 
-    it 'loads all of the reports into @reports' do
-      report1 = Report.create!
-      report2 = Report.create!
+    it 'returns no reports if report_params is blank' do
+      report_params = {}
       get :index
+      expect(assigns(:reports)).to be nil
+    end
 
-      expect(assigns(:reports)).to match_array([report1, report2])
+    it 'returns reports according to passed params' do
+      report1 = Report.where(symbol: 'AAPL').select(
+        :symbol, :return_on_capital, :earnings_yield
+      )
+      report2 = Report.where(symbol: 'TSRA').select(
+        :symbol, :return_on_capital, :earnings_yield
+      )
+
+      get :index, :sort_by => "return_on_capital", :limit => "1"
+
+      expect(assigns(:reports).first.attributes).to eq(report2.first.attributes)
     end
   end
 end
