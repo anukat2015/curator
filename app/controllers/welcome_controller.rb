9ee60bf82ad85ef322f82b36d9e55b2a8291b218
Corey Leveen <mntj11@gmail.com>
2015-02-28 19:02:16 -0500

    Fix index spec

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if !report_params.blank?
-      @reports = Report.select(:symbol, :return_on_capital, :earnings_yield)
-                       .order(report_params[:sort_by] => :desc)
+      @reports = Report.order(report_params[:sort_by] => :desc)
                        .limit(report_params[:limit].to_i)
 
       @message = MessageMaker.new(report_params).make_message
