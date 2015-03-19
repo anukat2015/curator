96d3a9f0ce506bd2ccc8418496d5342b58415641
Corey Leveen <mntj11@gmail.com>
2015-03-18 21:55:06 -0400

    Show the last time the data was updated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
     if !report_params.blank?
       @reports = Report.order(report_params[:sort_by] => :desc)
                        .limit(report_params[:limit].to_i)
-
       @message = MessageMaker.new(report_params).make_message
     end
+    @last_updated = Report.maximum("updated_at").to_date.to_s
   end
 
   private
