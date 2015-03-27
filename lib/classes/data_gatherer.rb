68dd4af94afb4fbb41e1042b44c1b6ce8db7bddd
Corey Leveen <mntj11@gmail.com>
2015-03-26 23:57:30 -0400

    Rename DataGatherer#filtered_data

--- lib/classes/data_gatherer.rb
+++ lib/classes/data_gatherer.rb
@@ -1,6 +1,6 @@
 class DataGatherer
   def gather_data
-    filtered_data
+    filter_data
   end
 
   private
@@ -11,7 +11,7 @@ class DataGatherer
 
   attr_reader :ticker_file
 
-  def filtered_data
+  def filter_data
     raw_company_data.keep_if do |obj|
       (obj.is_a? Hash) &&
       (obj[:return_on_capital].is_a? Float) &&
