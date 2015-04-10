eea178a85c85ced3522166a5334b3201cca50903
Corey Leveen <mntj11@gmail.com>
2015-04-09 23:44:35 -0400

    Extract Clerk case statement into format_values

--- lib/classes/clerk.rb
+++ lib/classes/clerk.rb
@@ -10,34 +10,11 @@ class Clerk
       reports.each do |report|
         report_info = []
         attrs.each do |attr|
-          case attr
-          when :symbol
-            report_info << report.symbol
-          when :name
-            report_info << report.name
-          when :return_on_capital
-            report_info << number_to_percentage(report.return_on_capital * 100, precision: 3)
-          when :earnings_yield
-            report_info << number_to_percentage(report.earnings_yield * 100,    precision: 3)
-          when :enterprise_value
-            report_info << number_to_currency(report.enterprise_value,          precision: 0)
-          when :ebit
-            report_info << number_to_currency(report.ebit,                      precision: 0)
-          when :market_cap
-            report_info << number_to_human(report.market_cap)
-          when :working_capital
-            report_info << number_to_currency(report.working_capital,           precision: 0)
-          when :fixed_assets
-            report_info << number_to_currency(report.fixed_assets,              precision: 0)
-          when :total_assets
-            report_info << number_to_currency(report.total_assets,              precision: 0)
-          when :current_assets
-            report_info << number_to_currency(report.current_assets,            precision: 0)
-          when :total_debt
-            report_info << number_to_currency(report.total_debt,                precision: 0)
-          when :cash_and_equivalents
-            report_info << number_to_currency(report.cash_and_equivalents,      precision: 0)
-          end
+          format_values(
+            report: report,
+            attribute: attr,
+            container: report_info
+          )
         end
         csv << report_info
       end
@@ -57,5 +34,36 @@ class Clerk
 
   def reports
     Report.order(params["sort_by"].intern => :desc).limit(params["limit"].to_i)
+  end
+
+  def format_values(report:, attribute:, container:)
+    case attribute
+    when :symbol
+      container << report.symbol
+    when :name
+      container << report.name
+    when :return_on_capital
+      container << number_to_percentage(report.return_on_capital * 100, precision: 3)
+    when :earnings_yield
+      container << number_to_percentage(report.earnings_yield * 100,    precision: 3)
+    when :enterprise_value
+      container << number_to_currency(report.enterprise_value,          precision: 0)
+    when :ebit
+      container << number_to_currency(report.ebit,                      precision: 0)
+    when :market_cap
+      container << number_to_human(report.market_cap)
+    when :working_capital
+      container << number_to_currency(report.working_capital,           precision: 0)
+    when :fixed_assets
+      container << number_to_currency(report.fixed_assets,              precision: 0)
+    when :total_assets
+      container << number_to_currency(report.total_assets,              precision: 0)
+    when :current_assets
+      container << number_to_currency(report.current_assets,            precision: 0)
+    when :total_debt
+      container << number_to_currency(report.total_debt,                precision: 0)
+    when :cash_and_equivalents
+      container << number_to_currency(report.cash_and_equivalents,      precision: 0)
+    end
   end
 end
