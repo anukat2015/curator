6a8e3fcce253947251170a8e2b96d6a14ada2cfb
Corey Leveen <mntj11@gmail.com>
2015-11-08 15:28:28 -0500

    Remove ReportsController, secret route

--- app/controllers/reports_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReportsController < ApplicationController
-  def get_reports
-    Overseer.new(ticker_file: "russell3000.txt").retrieve_and_persist_data
-    redirect_to :root
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
   get '/download', to: 'welcome#download_csv'
-
-  get "/#{ENV['SECRET_URI']}", to: 'reports#get_reports'
 end
